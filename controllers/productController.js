960324b8d80cab0c4045d029b1cb3eb230250f1a
Issarapap7 <film4777@gmail.com>
2022-04-27 01:26:45 +0700

    API edit Product

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const uuidv4 = require('uuid');
 const { promisify } = require('util');
+const e = require('express');
 const writeFileAsync = promisify(fs.writeFile);
 const readFileAsync = promisify(fs.readFile);
 
@@ -119,9 +120,41 @@ exports.getAllProduct = async (req, res, next) => {
     } catch (error) {
       next(error);  
     }
-
-    
 }
+
+exports.getProductByID = async (req, res, next) => {
+    try {
+        //const menu = await Menu.find();
+    const id  = req.params._id;
+    console.log('req.params = '+id);
+    // console.log(req.param._id.value);    
+    await Product
+    .findOne({_id:id})
+    .populate('typeSpecs')
+    .exec((err, type)=>{
+        if(err){ 
+            res.status(400).json({
+                data: "error"
+            })
+        }
+        console.log(type);
+        res.status(200).json({
+            
+                id: type._id,
+                type: type.typeSpecs,
+                price: type.price,
+                spec: type.spec,
+                img: "http://localhost:3000"+'/images/'+type.img,
+                number : type.number,
+                date: type.date
+        })
+    });
+    // console.log(product);
+    } catch (error) {
+      next(error);  
+    }
+}
+
 
 exports.updateProductStock = async (req, res ,next) => {
     try {
@@ -171,3 +204,56 @@ exports.updateProductStock = async (req, res ,next) => {
         next(error)
     }
 }
+
+
+exports.updateByOne = async (req, res, next) => {
+
+    try {
+        
+        // let product = new Product();
+        // product = req.body
+        const product = await Product.findOne({ _id: req.body.id })
+
+        if(!product){
+            const error = new Error('หาสินค้าที่จะแก้ไขไม่พบ');
+            error.statusCode = 400;
+            throw error;
+        }
+        
+
+
+        if(product){
+            console.log('เจอที่อยากอัพเดรต');
+            product.price = req.body.price;
+            product.number = req.body.number;
+            product.spec = req.body.spec;
+            
+            isBase64=Boolean;
+            var matches = req.body.img.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+            if (!matches || matches.length !== 3) {
+                isBase64 = false
+            }else{
+                isBase64 = true
+            }
+            
+            if(isBase64){
+                product.img = await saveImageToDisk(req.body.img)
+            }
+        }
+        
+        let result = await product.save();
+        
+        // const staff = await Product.findOneAndUpdate({_id : req.params._id}, req.body);
+
+        res.status(201).json({
+            data: result
+         })
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            message: "Can not update"
+        })
+    }
+
+ }
--- routes/product.js
+++ routes/product.js
@@ -6,5 +6,7 @@ const paassportJWT = require('../middleware/passportJWT');
 router.post('/addProduct',[paassportJWT.isLogin], productController.addProduct);
 router.get('/getallProduct',[paassportJWT.isLogin], productController.getAllProduct);
 router.put('/updateProductStock',[paassportJWT.isLogin], productController.updateProductStock);
+router.get('/getProductOne/:_id',[paassportJWT.isLogin], productController.getProductByID);
+router.put('/updateProductOne',[paassportJWT.isLogin], productController.updateByOne);
 
 module.exports = router;
